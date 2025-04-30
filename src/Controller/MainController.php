ca3687b07d961107f42aba07ccae55f060a34003
Glama107 <guigui.mareschal@gmail.com>
2025-04-30 10:16:34 +0200

    adding EasyAdminBundle

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,11 @@ use Symfony\Component\Routing\Attribute\Route;
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main')]
-    public function index(): Response
+    public function index(EventRepository $eventRepository): Response
     {
+        $groupedEvents = $eventRepository->findAllGroupedByDay();
         return $this->render('homepage.html.twig', [
-            'controller_name' => 'MainController',
+            'groupedEvents' => $groupedEvents,
         ]);
     }
 }
